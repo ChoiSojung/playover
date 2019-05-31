47d0af3c399ea95a06fe3c8ff3ab6985f04caf38
coultergeist <dcoult01@seattlecentral.edu>
2019-05-30 23:05:57 -0700

    New logo and styles

---
+++ app/src/main/java/com/playover/SplashScreen.java
@@ -0,0 +1,12 @@
+package com.playover;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
