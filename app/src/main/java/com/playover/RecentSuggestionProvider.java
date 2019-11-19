c7d230678cef13b5884d67b81427c075d8b06d7a
[choisojung] <[sojc31@gmail.com]>
2019-11-19 14:49:22 -0800

    updated my content provider with a constructor that implements recent query suggestions

--- app/src/main/java/com/playover/RecentSuggestionProvider.java
+++ app/src/main/java/com/playover/RecentSuggestionProvider.java
@@ -3,4 +3,10 @@ package com.playover;
 import android.content.SearchRecentSuggestionsProvider;
 
 public class RecentSuggestionProvider extends SearchRecentSuggestionsProvider {
+    public final static String AUTHORITY = "com.example.RecentSuggestionProvider";
+    public final static int MODE = DATABASE_MODE_QUERIES;
+
+    public RecentSuggestionProvider(){
+        setupSuggestions(AUTHORITY, MODE);
+    }
 }
