fb52015969af7ecd1352caa6b81680593fb6bd76
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-11-28 10:45:50 -0800

    Refactored selectedHotelTest

--- app/src/androidTest/java/com/playover/SelectedHotelTest.java
+++ app/src/androidTest/java/com/playover/SelectedHotelTest.java
@@ -20,6 +20,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.playover.MainActivityTest.withRecyclerView;
 import static org.junit.Assert.*;
 
 public class SelectedHotelTest {
@@ -31,42 +32,40 @@ public class SelectedHotelTest {
     @Rule
     public GrantPermissionRule locationCourseRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION);
 
-//    @Test
-//    public void testCheckOut() throws Exception {
-//        Thread.sleep(5000);
-//        try {
-//            onView(withId(R.id.main_content)).check(matches(isDisplayed()));
-//            onView(withId(R.id.main_content)).perform(DrawerActions.open());
-//            onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_sign_out));
-//            Thread.sleep(2000);
-//            onView(withId(R.id.email_login)).perform(replaceText("hotelTest@fake.com")).perform(ViewActions.closeSoftKeyboard());
-//            onView(withId(R.id.password_login)).perform(replaceText("Passw0rd!")).perform(ViewActions.closeSoftKeyboard());
-//            onView(withId(R.id.btn_login)).perform(click());
-//            Thread.sleep(5000);
-//            onView(withId(R.id.recycler_view_hotels))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-//            onView(withText("Confirm")).perform(click());
-//            Thread.sleep(5000);
-//            onView(withId(R.id.recycler_view_also_checked_in))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
-//            onView(withId(R.id.recycler_view_also_checked_in))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
-//            onView(withId(R.id.txtCheckoutSet)).perform(click());
-//        } catch (Exception ex) {
-//            onView(withId(R.id.lblogin_main)).perform(MainActivityTest.clickClickableSpan("Sign In"));
-//            onView(withId(R.id.email_login)).perform(replaceText("hotelTest@fake.com")).perform(ViewActions.closeSoftKeyboard());
-//            onView(withId(R.id.password_login)).perform(replaceText("Passw0rd!")).perform(ViewActions.closeSoftKeyboard());
-//            onView(withId(R.id.btn_login)).perform(click());
-//            Thread.sleep(5000);
-//            onView(withId(R.id.recycler_view_hotels))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-//            onView(withText("Confirm")).perform(click());
-//            Thread.sleep(5000);
-//            onView(withId(R.id.recycler_view_also_checked_in))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
-//            onView(withId(R.id.recycler_view_also_checked_in))
-//                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
-//            onView(withId(R.id.txtCheckoutSet)).perform(click());
-//            }
-//        }
+    @Test
+    public void testCheckOut() throws Exception {
+        Thread.sleep(5000);
+        try {
+            onView(withId(R.id.main_content)).check(matches(isDisplayed()));
+            onView(withId(R.id.main_content)).perform(DrawerActions.open());
+            onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_sign_out));
+            Thread.sleep(2000);
+            onView(withId(R.id.email_login)).perform(replaceText("hotelTest@fake.com")).perform(ViewActions.closeSoftKeyboard());
+            onView(withId(R.id.password_login)).perform(replaceText("Passw0rd!")).perform(ViewActions.closeSoftKeyboard());
+            onView(withId(R.id.btn_login)).perform(click());
+            Thread.sleep(5000);
+            onView(withId(R.id.recycler_view_also_checked_in))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
+            onView(withId(R.id.recycler_view_also_checked_in))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
+            onView(withRecyclerView(R.id.recycler_view_also_checked_in).atPositionOnView(0, R.id.checkbox))
+                    .perform(click());
+            Thread.sleep(3000);
+
+        } catch (Exception ex) {
+            onView(withId(R.id.lblogin_main)).perform(MainActivityTest.clickClickableSpan("Sign In"));
+            onView(withId(R.id.email_login)).perform(replaceText("hotelTest@fake.com")).perform(ViewActions.closeSoftKeyboard());
+            onView(withId(R.id.password_login)).perform(replaceText("Passw0rd!")).perform(ViewActions.closeSoftKeyboard());
+            onView(withId(R.id.btn_login)).perform(click());
+            Thread.sleep(5000);
+            onView(withId(R.id.recycler_view_also_checked_in))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
+            onView(withId(R.id.recycler_view_also_checked_in))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0,TestUtils.clickChildViewWithId(R.id.buddyStar)));
+            onView(withRecyclerView(R.id.recycler_view_also_checked_in).atPositionOnView(0, R.id.checkbox))
+                    .perform(click());
+            Thread.sleep(3000);
+
+            }
+        }
     }
